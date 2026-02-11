07c792ba764d885c811ae74ac146bf9ddf4b11ea
Dirk Eddelbuettel <edd@debian.org>
2026-02-10 20:54:28 -0600

    Add an else to is_root()
    But can we always run sudo?  Hm.

--- r-pkg/R/init.R
+++ r-pkg/R/init.R
@@ -2,6 +2,8 @@
 .onLoad <- function(libname, pkgname) {
     if (is_root()) {
         system2("apt", "update -qq")
-        refresh_cache()
+    } else {
+        system2("sudo", "apt update -qq")
     }
+    refresh_cache()
 }
